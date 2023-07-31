038e769a6718822909222367ceef6a5af2adda90
matigb160 <125412834+matigb160@users.noreply.github.com>
2023-07-31 16:35:21 -0600

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAlr6qbM1gxjt_kmAyV5PKA_1J3MhY7vhw",
+    authDomain: "kwitter-d4f47.firebaseapp.com",
+    projectId: "kwitter-d4f47",
+    storageBucket: "kwitter-d4f47.appspot.com",
+    messagingSenderId: "375343561989",
+    appId: "1:375343561989:web:28098707e8d54e492c0b6c"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
